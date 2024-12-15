fff54c5f369acd9ebfe6be33f4af0c426deefc65
Sara Romao <sara-romao@live.com>
2024-12-15 18:45:21 -0300

    feat: estrutura inicial form e calendario para cadastro

--- src/app/schedule/create/page.tsx
+++ src/app/schedule/create/page.tsx
@@ -1,9 +1,13 @@
 'use client'
+import Input from "@/components/form/input";
+import ClientSearchForm from "@/components/schedule/schedule-create-form";
 import { useState } from "react";
 import { DayPicker } from "react-day-picker";
+import 'react-day-picker/dist/style.css';
+
 
 export default function CreatePage(){
-    const [selected, setSelected] = useState<Date[] | undefined>();
+    const [selected, setSelected] = useState<Date[] | undefined>([]);
     const handleSelect = (newSelected:any) => {
        
         setSelected(newSelected);
@@ -13,13 +17,30 @@ export default function CreatePage(){
 
     
     return(
-        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-         <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <div className=" max-w-7xl mx-auto grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+         <main className="w-full flex flex-col gap-8 row-start-2 items-center sm:items-start">
          
             <h1 className="text-2xl font-bold mb-8"> Criar Agendamentos</h1>
+          <div className="w-full flex justify-between border">
+            
+            <div>
+                <h2>Informações Cliente</h2>
+                <div>
+                    <ClientSearchForm/>
+                </div>
 
-            <DayPicker  mode="multiple" selected={selected} onSelect={handleSelect} />
-            {/* {selected?.map((item, index) => <p key={index} className="mt-4">Data selecionada: {item.toDateString()}</p>)} */}
+               
+
+            </div>
+
+            <div>
+                <DayPicker  mode="multiple" selected={selected} onSelect={handleSelect} />
+            </div>
+          </div>
+            
+
+          
+            {selected?.map((item, index) => <p key={index} className="mt-4">Data selecionada: {item.toDateString()}</p>)}
          </main>
         </div>
         
