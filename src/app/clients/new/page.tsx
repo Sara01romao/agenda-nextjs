2e2321864ff32ff5d735687ed5be83a8b05945ab
Sara Romao <sara-romao@live.com>
2025-01-19 11:27:17 -0300

    style: melhoria layout botão voltar no form addCliente

--- src/app/clients/new/page.tsx
+++ src/app/clients/new/page.tsx
@@ -1,17 +1,21 @@
 import AddForm from "@/components/clients/AddForm";
 import Link from "next/link";
-import {PiUserPlusBold } from "react-icons/pi";
+import {PiArrowCircleLeft, PiUserPlusBold } from "react-icons/pi";
 
 export default function AddClient(){
 
     return(
         <div className="min-h-screen p-4 pb-20 pt-0 sm:p-4 max-[550px]:px-1 sm:pt-0 font-[family-name:var(--font-geist-sans)]">
-            <div className=" pt-4 px-3 flex flex-col mx-auto row-start-2 items-center sm:items-start max-w-5xl w-full">
-                <Link href={'/clients'} >Voltar</Link>
-            </div>
-            <main className="flex flex-col mx-auto row-start-2 items-center sm:items-start max-w-5xl w-full ">
+           
+            <main className="flex pt-4  flex-col mx-auto row-start-2 items-center sm:items-start max-w-5xl w-full ">
                 
                 <div className="bg-white rounded-xl  gap-5 w-full p-8 max-[550px]:px-1 max-sm:flex-col">
+                    <div className=" px-3 flex flex-col mx-auto row-start-2 items-center sm:items-start max-w-5xl w-full">
+                        <Link href={'/clients'} className="flex items-center gap-1 font-medium text-gray-600 hover:text-gray-800">
+                            <PiArrowCircleLeft size={30} className=""/>
+                            Voltar
+                        </Link>
+                    </div>
                     <h1 className="relative flex justify-center items-center gap-3 text-2xl font-semibold mb-5 mt-7 ml-5 mr-auto sm:text-left text-gray-800 before:absolute before:inset-x-0 before:bottom-[-5px] ">
                         
                         <PiUserPlusBold size={25}/>
