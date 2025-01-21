338aca65ebf66a9e7b2dd95590d337b56be5ab7a
Sara Romao <sara-romao@live.com>
2025-01-21 14:51:14 -0300

    bug: alterei rota do btn voltar

--- src/app/real-estate/edit/[id]/page.tsx
+++ src/app/real-estate/edit/[id]/page.tsx
@@ -27,7 +27,7 @@ export default async function EditSpace({ params }: SpaceIdParams) {
         <main className="flex pt-4  flex-col mx-auto row-start-2 items-center sm:items-start max-w-5xl w-full ">
             <div className="bg-white rounded-xl  gap-5 w-full p-8 max-[550px]:px-1 max-sm:flex-col">
                 <div className=" px-3 flex flex-col mx-auto row-start-2 items-center sm:items-start max-w-5xl w-full">
-                    <Link href={'/clients'} className="flex items-center gap-1 font-medium text-gray-600 hover:text-gray-800">
+                    <Link href={'/real-estate'} className="flex items-center gap-1 font-medium text-gray-600 hover:text-gray-800">
                         <PiArrowCircleLeft size={30} className=""/>
                         Voltar
                     </Link>
