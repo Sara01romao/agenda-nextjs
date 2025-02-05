f903a83e5b5edbc9d1fb9a3e33f79ade5c803dee
Sara Romao <sara-romao@live.com>
2025-02-04 23:24:01 -0300

    feat: criado rota de datas

--- src/app/api/schedule/route.ts
+++ src/app/api/schedule/route.ts
@@ -11,33 +11,51 @@ export interface ScheduleType extends RowDataPacket {
 
 }
 
-export async function GET() {
-    try {
-      const [rows] = await conn.execute<ScheduleType[]>(
-        `
-          SELECT 
-              agendamento.id_agendamento,
-              agendamento.datas_agendas,
-              agendamento.status_pagamento_agendamento,
-              agendamento.id_cliente_agendamento,
-              agendamento.id_espaco_agendamento,
-              clientes.nome_cliente
-          FROM 
-              agendamento
-          JOIN 
-              clientes 
-          ON 
-              agendamento.id_cliente_agendamento = clientes.id_cliente;
-        `
-      );
-  
- 
-      return NextResponse.json(rows, { status: 200 });
-    } catch (error) {
-      console.error("Erro ao buscar agendamentos:", error);
-      return NextResponse.json(
-        { error: "Erro ao buscar agendamentos" },
-        { status: 500 }
-      );
+export async function GET(req: Request) {
+  try {
+    const { searchParams } = new URL(req.url);
+    const onlyDates = searchParams.get("datas") === "true";
+
+    const [rows] = await conn.execute<ScheduleType[]>(
+      `
+        SELECT 
+            agendamento.id_agendamento,
+            agendamento.datas_agendas,
+            agendamento.status_pagamento_agendamento,
+            agendamento.id_cliente_agendamento,
+            agendamento.id_espaco_agendamento,
+            clientes.nome_cliente
+        FROM 
+            agendamento
+        JOIN 
+            clientes 
+        ON 
+            agendamento.id_cliente_agendamento = clientes.id_cliente;
+      `
+    );
+
+    const formattedData = rows
+    .map((row) => {
+      try {
+        return JSON.parse(row.datas_agendas); 
+      } catch {
+        return []; 
+      }
+    })
+    .flat();
+
+    if (onlyDates) {
+      return NextResponse.json(formattedData, { status: 200 });
     }
+
+    return NextResponse.json(rows, { status: 200 });
+  } catch (error) {
+    console.error("Erro ao buscar agendamentos:", error);
+    return NextResponse.json(
+      { error: "Erro ao buscar agendamentos" },
+      { status: 500 }
+    );
   }
+}
+
+
