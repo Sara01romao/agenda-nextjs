a5720bcb957dfe2dd9ebd97934f209896e3acb77
Sara Romao <sara-romao@live.com>
2025-08-04 23:41:50 -0300

    modified structure and files projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,28 @@
-// app/page.tsx
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Calender } from "./_components/calender";
+import Link from "next/link";
 
-import Calender from "@/components/calender";
-import CardDash from "@/components/cardDash";
-import { PiCalendarDots, PiChartLine, PiHouseLine, PiUsersThree } from "react-icons/pi";
-
-
-type Totals = {
-  agendamentos: number;
-  clientes: number;
-  imoveis: number;
-};
-
-
-
-
-export default async function Home() {
-    async function fetchTotals(): Promise<Totals> {
-        try {
-          const response = await fetch('http://localhost:3000/api/totals');
-          const result: Totals = await response.json();
-          return result;
-        } catch (error) {
-          console.error('Erro ao buscar dados:', error);
-          return {
-            agendamentos: 0,
-            clientes: 0,
-            imoveis: 0,
-          };
-        }
-      }
- 
-  const { agendamentos, clientes, imoveis } = await fetchTotals();
+export default function Home() {
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] justify-items-center min-h-screen p-8 pb-20 pt-0 sm:p-20 max-[550px]:px-1 sm:pt-0 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col row-start-2 items-center sm:items-start max-w-5xl w-full ">
-        <h1 className="relative text-2xl font-semibold mb-5 mt-7 ml-5 mr-auto sm:text-left text-gray-800 before:absolute before:inset-x-0 before:bottom-[-5px] before:h-1 before:bg-[#229BFF] before:w-full before:mx-auto">
-          Início
-        </h1>
+    <div className="font-sans  items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <h1>Agenda</h1>
 
-        <div className="bg-white rounded-xl flex flex-wrap gap-5 w-full p-8 max-[550px]:px-1 max-sm:flex-col">
-          <div className="mx-auto">
+      <Tabs defaultValue="casa" className="w-[1000px] p-20">
+        <TabsList>
+          <TabsTrigger value="casa">Casa </TabsTrigger>
+          <TabsTrigger value="sitio">Sítio Da Folha</TabsTrigger>
+        </TabsList>
+
+        <TabsContent value="casa" className="h-full border w-full px-8">
+          <h2 className="font-bold my-6">Datas Disponíveis</h2>
+          <div className="flex items-start gap-4">
             <Calender />
+            <Link href="/scheduling" className="flex bg-black text-white">Novo Agendamento</Link>
           </div>
-
-          <div className="mx-auto">
-            <h2 className="mb-8 text-2xl font-bold flex items-center gap-2">
-              <PiChartLine size={25} />
-              Informações
-            </h2>
-
-            <div className="flex flex-col gap-5 border-pink-100">
-              <CardDash icon={<PiCalendarDots size={25} />} title="Agendamentos" value={agendamentos} />
-              <div className="flex gap-3">
-                <CardDash icon={<PiUsersThree size={25} />} title="Clientes" value={clientes} />
-                <CardDash icon={<PiHouseLine size={25} />} title="Imóveis" value={imoveis} />
-              </div>
-            </div>
-          </div>
-        </div>
-      </main>
+        </TabsContent>
+        <TabsContent value="sitio">Change your password here.</TabsContent>
+      </Tabs>
     </div>
   );
 }
---
+++ src/app/scheduling/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Scheduling(){
+
+  return(
+    <div>
+      <Link href="/">Voltar</Link>
+    </div>
+  )
+}
