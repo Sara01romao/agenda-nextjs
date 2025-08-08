161f8d22280e8337499ee77e12ffb57ac11fe34d
Sara Romao <sara-romao@live.com>
2025-08-08 11:36:44 -0300

    modified structure to handle missing data property

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { TabsComponents } from "./_components/tabComponent";
 import { propertys } from "@/data/property-mock";
+import { BsHouse, BsHouseX } from "react-icons/bs";
 
 export default function Home() {
   const data = propertys;
 
-  if (!data) return <div className="h-[400px] w-full animate-pulse rounded-lg bg-gray-100 flex items-center justify-items-center"><p className="m-auto">Carregando...</p></div>;
+  if (!data) {
+    return (
+      <div className="h-[80vh] w-full rounded-lg bg-gray-100 flex items-center justify-center flex-col">
+        <BsHouseX  size={40}/>
+        <p className="text-lg font-medium text-gray-500">Nenhum Local Cadastrado</p>
+      </div>
+    )
+  }
 
   return (
     <div className="font-sans items-center justify-items-center min-h-screen  pb-20  sm:p-20">
