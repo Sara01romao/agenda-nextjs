4872d70dafee9d0e31c0773bb70f19f839dfce35
Sara Romao <sara-romao@live.com>
2025-09-30 23:12:55 -0300

    style: modified bg tabs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { TabsComponents } from "./_components/tabComponent";
 import { propertys } from "@/data/property-mock";
 import { BsHouse, BsHouseX } from "react-icons/bs";
+import clsx from "clsx";
 
 export default function Home() {
   const data = propertys;
@@ -9,7 +10,7 @@ export default function Home() {
   if (!data) {
     return (
       <div className="h-[80vh] w-full rounded-lg bg-gray-100 flex items-center justify-center flex-col">
-        <BsHouseX  size={40}/>
+        <BsHouseX size={40} />
         <p className="text-lg font-medium text-gray-500">Nenhum Local Cadastrado</p>
       </div>
     )
@@ -20,8 +21,17 @@ export default function Home() {
       <Tabs defaultValue="property-1" className="max-w-[1000px] w-full mb-10">
         <TabsList className="flex flex-wrap h-auto gap-2">
           {data.map(tabBtn =>
-            <TabsTrigger key={tabBtn.id} value={tabBtn.id}>{tabBtn.name}</TabsTrigger>
-          )}          
+            <TabsTrigger
+              key={tabBtn.id}
+              value={tabBtn.id}
+              className={clsx(
+                " rounded-md transition cursor-pointer",
+                "data-[state=active]:bg-black data-[state=active]:text-white",
+                "data-[state=inactive]:bg-gray-20"
+              )}
+
+            >{tabBtn.name}</TabsTrigger>
+          )}
         </TabsList>
 
         {data.map(tab =>
